72aa177e96f980876ceaa96632f454e87ed3f338
UsmanGhani6217 <bossbig6217@gmail.com>
2022-03-09 16:05:43 +0500

    updated the code structured and fixed use cases

--- src/components/products/index.test.tsx
+++ src/components/products/index.test.tsx
@@ -1,70 +1,82 @@
-import React from 'react'
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 // import react-testing methods
-import {render, getAllByTestId, waitForElementToBeRemoved, screen} from '@testing-library/react'
+import {
+  render,
+  waitForElementToBeRemoved,
+  screen,
+} from "@testing-library/react";
 
 // add custom jest matchers from jest-dom
-import '@testing-library/jest-dom'
-import {ProductsList} from './index';
+import "@testing-library/jest-dom";
+import { ProductsList } from "./index";
 
-const server = setupServer()
-describe("Given Notes",()=>{
-// Enable API mocking before tests.
-beforeAll(() => server.listen())
-// Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers())
-// Disable API mocking after the tests are done.
-afterAll(() => server.close());
+const server = setupServer();
+describe("Given Notes", () => {
+  // Enable API mocking before tests.
+  beforeAll(() => server.listen());
+  // Reset any runtime request handlers we may add during the tests.
+  afterEach(() => server.resetHandlers());
+  // Disable API mocking after the tests are done.
+  afterAll(() => server.close());
 
-test('WHEN product-list component is mounted THEN render the list of products', async()=>{
-  server.use(
-  rest.get('https://my-json-server.typicode.com/benirvingplt/products/products', (req, res, ctx) => {
-    return res(ctx.json([
-        {
-            "id": 1,
-            "colour": "Black",
-            "name": "Black Sheet Strappy Textured Glitter Bodycon Dress",
-            "price": 10,
-            "img": "http://cdn-img.prettylittlething.com/9/0/a/a/90aa90903a135ee59594f47c7685aa7ef3046e44_cly8063_1.jpg?imwidth=1024"
-          },
-    ]))
-  }),)
-render(<ProductsList />)
-await waitForElementToBeRemoved(() => screen.getByText("Loading..."));
-screen.getAllByTestId('product-item');
-});
-test("WHEN product-list api returns with no product THEN render no product data available text", async () => {
+  test("WHEN product-list component is mounted THEN render the list of products", async () => {
     server.use(
-      rest.get("https://my-json-server.typicode.com/benirvingplt/products/products", (req, res, ctx) => {
-        return res(ctx.json([]));
-      })
+      rest.get(
+        "https://my-json-server.typicode.com/benirvingplt/products/products",
+        (req, res, ctx) => {
+          return res(
+            ctx.json([
+              {
+                id: 1,
+                colour: "Black",
+                name: "Black Sheet Strappy Textured Glitter Bodycon Dress",
+                price: 10,
+                img: "http://cdn-img.prettylittlething.com/9/0/a/a/90aa90903a135ee59594f47c7685aa7ef3046e44_cly8063_1.jpg?imwidth=1024",
+              },
+            ])
+          );
+        }
+      )
+    );
+    render(<ProductsList />);
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+    screen.getAllByTestId("product-item");
+  });
+  test("WHEN product-list api returns with no product THEN render no product data available text", async () => {
+    server.use(
+      rest.get(
+        "https://my-json-server.typicode.com/benirvingplt/products/products",
+        (req, res, ctx) => {
+          return res(ctx.json([]));
+        }
+      )
     );
     render(<ProductsList />);
 
     // wait for Data to load
-    await waitForElementToBeRemoved(() =>screen.getByText("Loading..."));
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
 
     const noProductsListMessage = screen.getByText("No Product Available");
-
     expect(noProductsListMessage).toBeInTheDocument();
   });
-test("WHEN product-list api return error THEN render with error message text", async()=>{
+
+  test("WHEN product-list api return error THEN render with error message text", async () => {
     server.use(
-      rest.get("https://my-json-server.typicode.com/benirvingplt/products/products", (req, res, ctx) => {
-        return res(
+      rest.get(
+        "https://my-json-server.typicode.com/benirvingplt/products/products",
+        (req, res, ctx) => {
+          return res(
             ctx.status(404),
-            ctx.json({ message: 'Internal Server Error' }),
+            ctx.json({ message: "Internal Server Error" })
+          );
+        }
       )
-      })
     );
     render(<ProductsList />);
     // wait for Data to load
-    await waitForElementToBeRemoved(() =>screen.getByText("Loading..."));
-
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
     const errorMsg = screen.getByText("Something wen't wrong");
     expect(errorMsg).toBeInTheDocument();
-});
-
-
+  });
 });
--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -14,9 +14,7 @@ export const ProductsList = () => {
   });
   useEffect(() => {
     const fetchProducts = () => {
-      fetch(
-        `https://my-json-server.typicode.com/benirvingplt/products/products`
-      )
+      fetch(`https://my-json-server.typicode.com/benirvingplt/products/products`)
         .then((response) => response.json())
         .then((data) => {
           data.forEach(function (element: Product) {
@@ -30,55 +28,38 @@ export const ProductsList = () => {
   }, []);
   const getTotal = (data: Array<Product>, isFilter: boolean) => {
     let sum = 0;
+    let payload;
     for (let index = 0; index < data.length; index++) {
       const element = data[index];
       const quantity = element["quantity"] ? element.quantity : 0;
       const itemPrice = quantity * element.price;
       sum = sum + itemPrice;
     }
-
-    setState({ ...state, data, isFilter, sum });
-  };
-  const getTotalByFilter = (data: Array<Product>) => {
-    let sum = 0;
-    for (let index = 0; index < data.length; index++) {
-      const element = data[index];
-      const quantity = element["quantity"] ? element.quantity : 0;
-      const itemPrice = quantity * element.price;
-      sum = sum + itemPrice;
+    if (isFilter) {
+      payload = { ...state, filterItems: data, isFilter: true, sum };
+    } else {
+      payload = { ...state, data, isFilter, sum };
     }
-
-    setState({ ...state, filterItems: data, isFilter: true, sum });
+    setState(payload);
   };
-
-  const incrementHandler = (id: number) => {
-    const cloneData = state.data;
-    const newArr = cloneData.map((obj: any) => {
-      if (obj["id"] === id) {
-        return { ...obj, quantity: obj.quantity ? obj.quantity + 1 : 1 };
-      }
-      return obj;
-    });
-    getTotal(newArr, state.isFilter);
-  };
-  const decrementHandler = (id: number) => {
+  const countHandler = (type: string, id: number) => {
     const cloneData = [...state.data];
-    const newArr = cloneData.map((obj: any) => {
-      if (obj["id"] === id) {
-        return {
-          ...obj,
-          quantity: obj.quantity && obj.quantity > 0 ? obj.quantity - 1 : 0,
-        };
+    cloneData.forEach((item) => {
+      if (item.id === id && type === "increment") {
+        item.quantity = item.quantity ? item.quantity + 1 : 1;
+      } else if (item.id === id && type === "decrement") {
+        item.quantity =
+          item.quantity && item.quantity > 0 ? item.quantity - 1 : 0;
       }
-      return obj;
     });
-    getTotal(newArr, state.isFilter);
+    const payload = state.isFilter ? state.filterItems : cloneData;
+    getTotal(payload, state.isFilter);
   };
   const changeHandler = (e: any) => {
     if (e.currentTarget.value) {
       const data = state.data;
       const result = data.filter((x) => x.colour === e.currentTarget.value);
-      getTotalByFilter(result);
+      getTotal(result, true);
     } else {
       getTotal(state.data, false);
     }
@@ -87,9 +68,13 @@ export const ProductsList = () => {
     const data = state.isFilter ? state.filterItems : state.data;
     if (data.length === 0) return <div>No Product Available</div>;
     return data.map((product: Product) => (
-      <div data-testid="product-item" key={product.id} className="Cart-Items">
+      <div
+        data-testid="product-item"
+        key={product.id}
+        className=" mx-auto Cart-Items"
+      >
         <div className="image-box">
-          <img src={product.img} style={{ height: "120px" }} />
+          <img src={product.img} style={{ width: "120px", height: "120px" }} />
         </div>
         <div className="about">
           <p className="title">{product.name}</p>
@@ -98,18 +83,18 @@ export const ProductsList = () => {
         <div className="counter">
           <div
             onClick={() => {
-              decrementHandler(product.id);
+              countHandler("decrement", product.id);
             }}
-            className="btn"
+            className="btn btn-danger"
           >
             -
           </div>
           <div className="count">{product.quantity}</div>
           <div
             onClick={() => {
-              incrementHandler(product.id);
+              countHandler("increment", product.id);
             }}
-            className="btn"
+            className="btn btn-success"
           >
             +
           </div>
@@ -135,27 +120,31 @@ export const ProductsList = () => {
     <main className="wrapper">
       <div className="CartContainer">
         <div className="Header">
-          <h3 className="Heading">Shopping Cart</h3>
+          <h1 className="text-center">Shopping Cart</h1>
+          <div className="custom-select">
+          <select
+            onChange={changeHandler}
+            className="form-select mb-10"
+            aria-label="Default select example"
+          >
+            <option value="">Select one color</option>
+            <option value="Red">Red</option>
+            <option value="Black">Black</option>
+            <option value="Stone">Stone</option>
+          </select>
+          </div>
         </div>
-        <select
-          onChange={changeHandler}
-          className="form-select"
-          aria-label="Default select example"
-        >
-          <option value="">Select one color</option>
-          <option value="Red">Red</option>
-          <option value="Black">Black</option>
-          <option value="Stone">Stone</option>
-        </select>
+
         {renderProducts()}
         <div className="checkout">
           <div className="total">
             <div>
               <div className="Subtotal">Total</div>
             </div>
-            <div className="total-amount">${state.sum}</div>
+            <div className="total-amount">
+              ${state.sum ? state.sum.toFixed(2) : 0}
+            </div>
           </div>
-          <button className="button">Checkout</button>
         </div>
       </div>
     </main>
